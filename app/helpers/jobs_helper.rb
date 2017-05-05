8f2634818a978450210a655b45acdec8771b2573
dumpling001 <dumpling001@gmail.com>
2017-05-05 11:27:55 +0800

    fixed job#show ref to Xdite's 66Kjobs

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -21,13 +21,37 @@ module JobsHelper
     end
   end
 
-
   def render_voter_status(job)
      if current_user && current_user.is_voter_of?(job)
        link_to("", downvote_job_path(job), method: :post, class: "fa fa-heart", style: "color:red;")
          else
        link_to("", upvote_job_path(job), method: :post, class: "fa fa-heart-o")
      end
+  end
+
+  def render_job_description(job)
+    simple_format(job.description)
+  end
+
+  def render_job_warning(salary)
+    if salary < 5000
+      "[最低薪 < 5000 請自行斟酌是否應徵此公司]"
+    end
+  end
+
+  def render_job_salary(salary)
+
+    special_label = if salary >= 5000
+      "label-danger"
+    elsif salary >= 10000
+      "label-success"
+    else
+      ""
+    end
+
+    salary = number_to_currency(salary, precision: 0)
+
+    content_tag(:span, "人民币 #{salary} 元", :class => "label label-default #{special_label}")
   end
 
 end
